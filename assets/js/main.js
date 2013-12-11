8a34f64e14ff24316fa1bf2d527e2ec1c742cf73
angelfire <andres.paranoid@gmail.com>
2013-12-11 14:31:31 -0500

    New implementacion - Backbone & Require

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,23 @@
-requirejs.config({
-	"baseUrl": "assets/js",
-	"paths": {
-		"jquery": "//code.jquery.com/jquery-1.10.2.min"
-	}
+require.config({
+	'baseUrl': 'assets/js',
+	'paths': {
+    // define vendor paths
+		'jquery': '//code.jquery.com/jquery-1.10.2.min',
+    'underscore': 'vendor/underscore-min',
+    'backbone': 'vendor/backbone-min'    
+	},
+
+  // shim declaration
+  shim: {
+    'underscore': {
+      'exports': '_'  
+    },
+    'backbone': {
+      'deps': ['jquery', 'underscore'],
+      'exports': 'Backbone'
+    }
+  }
+
 });
 
-requirejs(["agenda"]);
+requirejs(['agenda']);
