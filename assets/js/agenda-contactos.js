3890ba2b5a88de573a2a7379ebbe7cfae2298fef
angelfire <andres.paranoid@gmail.com>
2013-12-17 16:36:48 -0500

    first commit

--- assets/js/agenda-contactos.js
+++ assets/js/agenda-contactos.js
@@ -1,71 +1,78 @@
-define(['jquery', 'underscore', 'backbone'], function($, _, Backbone){
-  // variables
-  var AgendaModel = AgendaModel || {},
-      AgendaCollection = AgendaCollection || {},
-      AgendaView = AgendaView || {};
+// variables
+var AgendaModel = AgendaModel || {},
+    AgendaCollection = AgendaCollection || {},
+    AgendaView = AgendaView || {};
 
-  // Model
-  AgendaModel = Backbone.Model.extend({
-    initialize: function(){
-      console.log('Agenda Model has been initialized');
-    },
-    defaults: function(){
-      return{
-        nombre: '',
-        telefono: '',
-        email: ''
-      };
+// Model
+AgendaModel = Backbone.Model.extend({
+  initialize: function(){
+    console.log('Agenda Model has been initialized');
+  },
+
+  defaults: function(){
+    return{
+      nombre: 'Andres',
+      telefono: '1234567',
+      email: 'andres.bedoya@globant.com'
+    };
+  },
+
+  validate: function(attributes){
+    if (!_.isString(attributes.nombre) || !attributes.name){
+      return "Solo se permiten letras";
     }
-  });
-
-  var agendamodel = new AgendaModel();
-
-  // Collection
-  AgendaCollection = Backbone.Collection.extend({
-    initialize: function(){
-      console.log("Agenda Collection has been initialized");
-    },
-    model: AgendaModel
-  });
-
-  var agendacollection = new AgendaCollection();
-
-  // View
-  AgendaView = Backbone.View.extend({
-    el: $('#container'),
-
-    initialize: function(){
-      _.bindAll(this);
-    },
-
-    tempCont: _.template($('#lista-contactos').html()),
-
-    events: {
-      'click .btn-lg': 'add',
-      'click .btnEdit': 'edit',
-      'click .btnDelete': 'delete'
-    },
-
-    add: function(){
-      var newContact = new AgendaModel({
-        nombre: $('nombre').val(),
-        telefono: $('telefono').val(),
-        email: $('email').val()
-      });
-      agendacollection.add(JSON.stringify(newContact));
-    },
-
-    edit: function(){
-
-    },
-
-    delete: function(){
-
+    if (!_.isNumber(attributes.telefono)){
+      return "Solo se permiten números";
     }
+  }
+});
 
+// Collection
+AgendaCollection = Backbone.Collection.extend({
+  initialize: function(){
+    console.log("Agenda Collection has been initialized");
+  },
+  model: AgendaModel
+});
 
-  });
+// View
+AgendaView = Backbone.View.extend({
+  el: '#container',
 
-  var agendaview = new AgendaView();
+  tempCont: _.template($('#lista-contactos').html()),
+
+  initialize: function(){
+    this.render();
+  },
+
+  render: function(){
+    this.$el.html(this.tempCont(this.model.toJSON()));
+  },  
+
+  events: {
+    'click .btn-lg': 'add',
+    'click .btnEdit': 'edit',
+    'click .btnDelete': 'delete'
+  },
+
+  add: function(){
+    var newContact = new AgendaModel({
+      nombre: $('nombre').val(),
+      telefono: $('telefono').val(),
+      email: $('email').val()
+    });
+    agendacollection.add(JSON.stringify(newContact));
+  },
+
+  edit: function(){
+
+  },
+
+  delete: function(){
+
+  }
 
 });
+
+var agendamodel = new AgendaModel();
+var agendaview = new AgendaView({ model: agendamodel });
