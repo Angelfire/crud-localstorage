2e2cef6d8b11af37c236b83492bf6035986c9c86
angelfire <andres.paranoid@gmail.com>
2013-12-19 11:39:24 -0500

    backbone namespacing

--- assets/js/agenda-contactos.js
+++ assets/js/agenda-contactos.js
@@ -1,79 +1,100 @@
-// variables
-var AgendaModel = AgendaModel || {},
-    AgendaCollection = AgendaCollection || {},
-    AgendaView = AgendaView || {};
+(function(){ 
+  // global variables - namespacing
+  window.App = {
+    Models: {},
+    Views: {},
+    Collections: {}
+  };
 
-// Model
-AgendaModel = Backbone.Model.extend({
-  initialize: function(){
-    console.log('Agenda Model has been initialized');
-  },
+  // Model for a Contact
+  App.Models.Contacto = Backbone.Model.extend({
+    defaults: {
+        nombre: 'Andres',
+        telefono: '1234567',
+        email: 'andres.bedoya@globant.com'
+    },
 
-  defaults: {
-      id: 1,
-      nombre: 'Andres',
+    validate: function(attributes){
+      if (!_.isString(attributes.nombre) || !attributes.name){
+        return "Solo se permiten letras";
+      }
+      if (!_.isNumber(attributes.telefono)){
+        return "Solo se permiten números";
+      }
+    }
+  });
+
+  // Collection for all Contacts
+  App.Collections.Contactos = Backbone.Collection.extend({
+    model: App.Models.Contacto,
+    localStorage: new Backbone.LocalStorage("Contactos")
+  });
+
+  // View for all Contacts
+  App.Views.Contactos = Backbone.View.extend({
+    el: $('#contactos-agenda').find('tbody'),
+
+    render: function(){      
+      console.log(this.collection.each);
+    }
+
+  });
+
+  // View for one Contacts
+  App.Views.Contacto = Backbone.View.extend({
+    el: $('#contactos-agenda'),
+
+    template: _.template($('#lista-contactos').html()),
+
+    initialize: function(){
+      this.render();
+    },
+
+    render: function(){
+      var tbody = this.$el.find("tbody");//.find('tr');
+      tbody.empty();
+      tbody.append(this.template(this.model.toJSON()));      
+      return this;
+    },
+
+    events: {
+      'click .btn-lg': 'add',
+      'click .btnEdit': 'edit',
+      'click .btnDelete': 'delete'
+    },
+
+    add: function(){
+      var newContact = new App.Models.Contacto({
+        nombre: $('nombre').val(),
+        telefono: $('telefono').val(),
+        email: $('email').val()
+      });
+    },
+
+    edit: function(){
+
+    },
+
+    delete: function(){
+      this.model.destroy();
+      this.remove();
+    }
+
+  });
+
+  var contactoCollection = new App.Collections.Contactos([
+    {
+      nombre: 'Sergio',
       telefono: '1234567',
       email: 'andres.bedoya@globant.com'
-  },
-
-  validate: function(attributes){
-    if (!_.isString(attributes.nombre) || !attributes.name){
-      return "Solo se permiten letras";
+    },
+    {
+      nombre: 'Laura',
+      telefono: '1234567',
+      email: 'andres.bedoya@globant.com'
     }
-    if (!_.isNumber(attributes.telefono)){
-      return "Solo se permiten números";
-    }
-  }
-});
+  ]);
+  
+  var contactosView = new App.Views.Contactos({ collection: contactoCollection });
 
-// Collection
-AgendaCollection = Backbone.Collection.extend({
-  initialize: function(){
-    console.log("Agenda Collection has been initialized");
-  },
-  model: AgendaModel
-});
-
-// View
-AgendaView = Backbone.View.extend({
-  el: '#contactos-agenda',
-
-  template: _.template($('#lista-contactos').html()),
-
-  initialize: function(){
-    this.render();
-  },
-
-  render: function(){
-    var tbody = this.$el.find("tbody");
-    tbody.empty();
-    tbody.append(this.template(this.model.toJSON()));
-  },  
-
-  events: {
-    'click .btn-lg': 'add',
-    'click .btnEdit': 'edit',
-    'click .btnDelete': 'delete'
-  },
-
-  add: function(){
-    var newContact = new AgendaModel({
-      nombre: $('nombre').val(),
-      telefono: $('telefono').val(),
-      email: $('email').val()
-    });
-    agendacollection.add(JSON.stringify(newContact));
-  },
-
-  edit: function(){
-
-  },
-
-  delete: function(){
-
-  }
-
-});
-
-var agendamodel = new AgendaModel();
-var agendaview = new AgendaView({ model: agendamodel });
+})();
